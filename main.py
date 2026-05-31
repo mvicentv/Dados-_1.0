7a9ac340ffc1b6b97d29577a3a8e19aae9a80e74
mvicentv <miguel.vicente.vicente@gmail.com>
2026-05-31 16:05:51 +0200

    Añadir código de lanzamiento de dados

---
+++ main.py
@@ -0,0 +1,29 @@
+
+import time
+import dice
+
+
+def roll_dice(amount: int, sides: int) -> list:
+    """
+    Devuelve una lista de tamaño 'amount' con los resultados
+    de lanzar un dado de 'sides' caras.
+    """
+    results = dice.roll(f"{amount}d{sides}")
+    return list(results)
+
+
+def main():
+    amount = 5
+    sides = 6
+    
+
+    results = roll_dice(amount, sides)
+
+    for i, result in enumerate(results, start=1):
+        print(f"Lanzamiento {i} número obtenido {result}")
+        if i < len(results):
+            time.sleep(5)
+
+
+if __name__ == "__main__":
+    main()
